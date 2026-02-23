9a9c2104401a304ac949fcab9a7b39b2cb6020fd
Hector <noctisatrae@pm.me>
2026-02-22 20:36:01 -0500

    feat: use geist as monofont

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "bilduuuuung.work";
-export const SITE_DESCRIPTION = "Hector's crazy website!";
+export const SITE_TITLE = "bilduuuung.work";
+export const SITE_DESCRIPTION = "Hector's crazy and awesome website";
