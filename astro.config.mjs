4ebc28a5a10681f3a2ac68b1464d514afca8c40b
Hector <noctisatrae@pm.me>
2026-02-22 23:35:12 -0500

    fix: last touchups

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import remarkGfm from "remark-gfm";
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "");
+const ngrokHost = env.NGROK_HOST;
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,7 +24,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
     server: {
-      allowedHosts: ["4b09210dcf51.ngrok.app"],
+      allowedHosts: ngrokHost ? [ngrokHost] : [],
     },
   },
 });
